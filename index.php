f1008c6b263a2c07b9dae892bfdcfea9fa6121a3
emiliev <emil_iliev1994@abv.bg>
2016-06-27 23:37:24 +0300

    test query

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
+	require_once("db.php");
+
+	$query = "SELECT * FROM products";
+	$result = $conn->query($query);
+	while ($row = $result->fetch_array()) {
+			echo $row["description"] . " " . $row["price"];
+			echo "<br />";
+	}
 	echo "Hello";
 ?>
