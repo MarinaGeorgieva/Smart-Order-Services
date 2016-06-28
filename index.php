78966dd2018d674a214c14aafc91a3362dc3b7df
emiliev <emil_iliev1994@abv.bg>
2016-06-28 17:58:49 +0300

    Query test

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 	require_once("db.php");
+?>
 
+<?php
 	$query = "SELECT * FROM products";
 	$result = $conn->query($query);
 	while ($row = $result->fetch_array()) {
@@ -8,4 +10,20 @@
 			echo "<br />";
 	}
 	echo "Hello";
+?>
+
+
+<?php
+
+	 	$sql = "INSERT INTO products (description, price, category";
+		$sql .= ") VALUES ('fanta lemon', 1.70, 'drinks')";
+		$result = $conn->query($sql);
+		if($result){
+			echo "New record created successfully!";
+		}
+		else{
+
+			echo "Error!" . $sql . "<br />" . $conn->error;
+		}
+
 ?>
