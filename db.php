81fba14d78f8e2f3ecf0761f370b270b4aa796f8
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2016-06-29 23:49:23 +0300

    Changed db connection in order to deploy

--- db.php
+++ db.php
@@ -1,14 +1,18 @@
 <?php
 
 require_once 'config.php';
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
+// If working with heroku
 
-// $conn = new mysqli($server, $username, $password, $db);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$connection = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASS);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$connection = new PDO("mysql:host=".$server.";dbname=".$db, $username, $password);
+
+// Uncomment if working locally with phpmyadmin
+// $connection = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASS);
 ?>
