dd8463e44058b88af0c483920290dc5171bd76bf
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2016-07-06 07:14:35 +0300

    Fixed config.php to work with heroku

--- config.php
+++ config.php
@@ -3,19 +3,19 @@
 // If working locally
 
 // Database Constants
-defined("DB_SERVER")? null : define("DB_SERVER", "localhost");
-defined("DB_USER") ? null : define("DB_USER", "waiter");
-defined("DB_PASS") ? null : define("DB_PASS", "12345"); 
-defined("DB_NAME") ? null : define("DB_NAME", "smartorder");
+// defined("DB_SERVER")? null : define("DB_SERVER", "localhost");
+// defined("DB_USER") ? null : define("DB_USER", "waiter");
+// defined("DB_PASS") ? null : define("DB_PASS", "12345"); 
+// defined("DB_NAME") ? null : define("DB_NAME", "smartorder");
 
 
 // If working with heroku
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// defined("DB_SERVER")? null : define("DB_SERVER", $url["host"]);
-// defined("DB_USER") ? null : define("DB_USER", $url["user"]);
-// defined("DB_PASS") ? null : define("DB_PASS", $url["pass"]); 
-// defined("DB_NAME") ? null : define("DB_NAME", substr($url["path"], 1));
+defined("DB_SERVER")? null : define("DB_SERVER", $url["host"]);
+defined("DB_USER") ? null : define("DB_USER", $url["user"]);
+defined("DB_PASS") ? null : define("DB_PASS", $url["pass"]); 
+defined("DB_NAME") ? null : define("DB_NAME", substr($url["path"], 1));
 
 ?>
